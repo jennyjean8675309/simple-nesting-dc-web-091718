308e36da6aee0f7cfd99ed7ab928fd964bdc43b8
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-05 18:07:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -105,8 +105,6 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
-     
-     
 end
 
 def adding_to_dennis
@@ -125,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash
 end
